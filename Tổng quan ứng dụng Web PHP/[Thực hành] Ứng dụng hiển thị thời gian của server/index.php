bbca5a8a28c3d58c90631461c3e0aad0f14350a9
Your Name <you@example.com>
2021-05-12 22:44:29 +0700

    [Thực hành] Ứng dụng hiển thị thời gian của server

---
+++ Tổng quan ứng dụng Web PHP/[Thực hành] Ứng dụng hiển thị thời gian của server/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Thời gian hiện tại</title>
+</head>
+<body>
+<h1> Bây giờ là: <?php echo date('Y-M-d h:i:s'); ?> </h1>
+</body>
+</html>
+<?php date_default_timezone_set('Asia/Ho_Chi_Minh') ?>
