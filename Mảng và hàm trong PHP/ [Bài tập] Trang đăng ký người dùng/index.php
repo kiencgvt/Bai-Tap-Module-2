b31f92e228d144a993f7144b8270536c84ef8b06
Your Name <you@example.com>
2021-05-14 15:44:47 +0700

    [Bài tập] Trang đăng ký người dùng

--- Mảng và hàm trong PHP/ [Bài tập] Trang đăng ký người dùng/index.php
+++ Mảng và hàm trong PHP/ [Bài tập] Trang đăng ký người dùng/index.php
@@ -8,27 +8,49 @@
     <title>Document</title>
 </head>
 <body>
-<form action="" method="post">
-    User Name: <input type="text" name="userName"><br>
-    Email: <input type="text" name="email"><br>
-    Phone Number: <input type="number" name="phoneNumber">
+<form method="post">
+    Tên người dùng: <input type="text" name="username">
+    Email: <input type="text" name="email">
+    Điện thoại: <input type="text" name="phoneNumber">
+    <input type="submit" value="Gửi đi">
 </form>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $userName = $_POST["userName"];
-    $email = $_POST["email"];
-    $phoneNumber = $_POST["phoneNumber"];
-}
-if (empty($userName) || empty($email) || empty($phoneNumber)) {
-    echo "Không được để trống thông tin";
-}
-else {
-    if (filter_var($email, 274)) {
-
+    //Lấy dữ liệu từ form
+    $username = $_REQUEST["username"];
+    $email = $_REQUEST["email"];
+    $phoneNumber = $_REQUEST["phoneNumber"];
+    if (empty($username) || empty($email) || empty($phoneNumber)) {
+        echo "Không được để thông tin rỗng";
     }
-}
-function saveDataJSON(filename, $name, $email, $phone) {
+    else {
 
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            saveDataJSON("users.json", $username, $email, $phoneNumber);
+        }
+        else {
+            echo "Định dạng email không đúng!";
+        }
+    }
+    function saveDataJSON($filename, $username, $email, $phoneNumber) {
+        $dataCurrent = getDataJSON($filename);
+        $contact = ["username" => $username,
+            "email" => $email,
+            "phoneNumber" => $phoneNumber
+        ];
+        $dataCurrent[] = $contact;
+        //Hàm json_encode là hàm chuyển mảng về JSON
+        $contactJSON = json_encode($contact);
+        //Lưu dữ liệu vào file
+        file_put_contents($filename, $contactJSON);
+    }
+    function getDataJSON($filename) {
+        //Đọc dữ liệu từ file dùng hàm file_get_contents, dữ liệu trả về thuộc kiểu dữ liệu JSON
+        $dataIntoFile = file_get_contents($filename);
+        //Chuyển dữ liệu từ JSON về mảng dùng hàm json_decode
+        $dataArray = json_decode($dataIntoFile, true);
+        return $dataArray;
+    }
 }
 ?>
 </body>
