72cb164760ffc4bf091dd1d667fbf1051b2cdd6a
Your Name <you@example.com>
2021-05-17 10:08:24 +0700

    [Thực hành] Lớp hình chữ nhật

---
+++  Lớp và đối tượng trong PHP/[Thực hành] Lớp hình chữ nhật/index.php
@@ -0,0 +1,21 @@
+<?php
+include_once "HCN.php";
+$width = 10;
+$height = 20;
+$rectangle = new Rectangle($width, $height);
+echo $rectangle->width; // 0utput:
+echo "<br>";
+echo $rectangle->height; // 0utput: 20
+echo "<br>";
+$rectangle->height = 30;
+$rectangle->width = 20;
+
+echo $rectangle->height; // 0utput: 30
+echo "<br>";
+echo $rectangle->width; // 0utput: 20
+echo "<br>";
+echo $rectangle->getPerimeter(); // 0utput: 100
+echo "<br>";
+echo $rectangle->getArea(); // Output: 600
+echo "<br>";
+echo ("Your Rectangle". $rectangle->display());
